71e757e8f83fa5b2210fcd766907e2466343bdee
Owen Sesaldo <owen.sesaldo@gmail.com>
2018-05-01 23:15:36 +0800

    Update: Homepage CSS

--- src/assets/js/owl-carousel-custom.js
+++ src/assets/js/owl-carousel-custom.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     nav:true,
-    navText: ["<img src='./assets/img/img-home-arrow-left.png'>","<img src='./assets/img/img-home-arrow-right.png'>"],
+    navText: ["<img src='wordoftravel/assets/img/img-home-arrow-left.png'>","<img src='wordoftravel/assets/img/img-home-arrow-right.png'>"],
     dots:false,
     loop:true,
     margin:10,
     responsiveClass:true,
     responsive:{
         0:{
-            items:1 ,
+            items:2,
         },
         600:{
             items:4,
