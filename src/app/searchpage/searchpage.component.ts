471fc2f142e3a0bb13cc332439a0993ef2c6774f
Jaime R. Gaffod Jr <jgaffod.itc@gmail.com>
2018-05-07 17:29:07 +0800

    date and comma solve

--- src/app/searchpage/searchpage.component.ts
+++ src/app/searchpage/searchpage.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChildren, ChangeDetectorRef } from '@angular/cor
 import { SearchPageService } from './search-page.service';
 import { SearchPage } from './searchpage';
 
-import {ActivatedRoute, Router} from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { SearchService } from '../travel-search/search.service';
 import { tap, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
 import { Subject } from 'rxjs/Subject';
@@ -20,10 +20,10 @@ export class SearchpageComponent implements OnInit {
   searchTerm: any;
   searchName: any;
   // for suggestions
-	suggestions = [];
-	suggestionsLoading = false;
-	suggestionTypeahead = new Subject<string>();
-	selectedSuggestion;
+  suggestions = [];
+  suggestionsLoading = false;
+  suggestionTypeahead = new Subject<string>();
+  selectedSuggestion;
 
 
   @ViewChildren('isotopeitems') items: any;
@@ -32,41 +32,41 @@ export class SearchpageComponent implements OnInit {
     private searchApiService: SearchPageService,
     private route: ActivatedRoute,
     private searchService: SearchService,
-		private cd: ChangeDetectorRef,
+    private cd: ChangeDetectorRef,
     private router: Router,
-    ) { 
-    this.route.params.subscribe( params => {
+  ) {
+    this.route.params.subscribe(params => {
       this.searchTerm = params.term;
-      this.searchName = this.searchTerm.split(/[0-9.\-_]+/);
-      
+      this.searchName = this.searchTerm.split(/[0-9\-_]+/).join('');
+
     });
   }
   ngOnInit() {
     this.getSearchResult();
     // typehead for pipe
-		this.suggestionTypeahead.pipe(
-			tap(() => this.suggestionsLoading = true),
-			distinctUntilChanged(),
-			debounceTime(200),
-			switchMap(term => this.searchService.searchSuggestions(term)),
-		).subscribe(res => {
-			if (
-				res.hasOwnProperty('suggest') &&
-				res.suggest.hasOwnProperty('alternateNames-suggestion') &&
-				res.suggest['alternateNames-suggestion'].length > 0 &&
-				res.suggest['alternateNames-suggestion'][0].hasOwnProperty('options')
-			) {
-				const value = res.suggest['alternateNames-suggestion'][0].options;
-				this.suggestions = value;
-			} else {
-				this.suggestions = [];
-			}
-			this.suggestionsLoading = false;
-			this.cd.markForCheck();
-		}, () => {
-			//this.suggestions = [];
-			//this.suggestionsLoading = false;
-		});
+    this.suggestionTypeahead.pipe(
+      tap(() => this.suggestionsLoading = true),
+      distinctUntilChanged(),
+      debounceTime(200),
+      switchMap(term => this.searchService.searchSuggestions(term)),
+    ).subscribe(res => {
+      if (
+        res.hasOwnProperty('suggest') &&
+        res.suggest.hasOwnProperty('alternateNames-suggestion') &&
+        res.suggest['alternateNames-suggestion'].length > 0 &&
+        res.suggest['alternateNames-suggestion'][0].hasOwnProperty('options')
+      ) {
+        const value = res.suggest['alternateNames-suggestion'][0].options;
+        this.suggestions = value;
+      } else {
+        this.suggestions = [];
+      }
+      this.suggestionsLoading = false;
+      this.cd.markForCheck();
+    }, () => {
+      //this.suggestions = [];
+      //this.suggestionsLoading = false;
+    });
   }
 
   ngAfterViewInit() {
@@ -84,13 +84,13 @@ export class SearchpageComponent implements OnInit {
         error => this.errorMessage = <any>error);
   }
   onClickSearch() {
-		const selected = this.selectedSuggestion;
-		if (!selected) {
-			// TODO error handling
-			return
-		}
-    this.router.navigate(['/wordoftravel/destination/',selected.text + "-" + selected._id]);
+    const selected = this.selectedSuggestion;
+    if (!selected) {
+      // TODO error handling
+      return
+    }
+    this.router.navigate(['/wordoftravel/destination/', selected.text + "-" + selected._id]);
     this.getSearchResult();
-	}
+  }
 
 }
