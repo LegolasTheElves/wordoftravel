24c2df4885c6e4bcdeea1652c62c3f8c3594f875
Jaime R. Gaffod Jr <jgaffod.itc@gmail.com>
2018-05-12 19:02:41 +0800

    searchpage thumbnail/image clickble

--- src/app/searchpage/sub/details/details.ts
+++ src/app/searchpage/sub/details/details.ts
@@ -2,13 +2,23 @@ export interface SearchDetails{
     Title: string,
     Blogger: string,
     UpdatedDate: string,
+    LanguageDetected: string,
+    ImageExt: string,
+    Tags: string[],
     PostURL: string,
     Description: string,
     RatingLikes: number,
     BloggerImage: string,
     ThumbnailImage: string,
-    Places: string,
-    Topics: string,
+    Places: [{
+        Priority: number,
+        GeoNamesID: string,
+        LocationName: string,
+        Location: string
+
+    }],
+    Topics: string[],
+    ReadingTime: number,
     BlogTitle: string,
     BlogDescription: string
 }
