ae5f38d5e802bb6348a1e6e8dc57b38e395f2b1c
Jaime R. Gaffod Jr <jgaffod.itc@gmail.com>
2018-05-16 22:21:58 +0800

    Homepage and Explore Destination to specific destination

--- src/app/specific-destination/specific-destination.component.ts
+++ src/app/specific-destination/specific-destination.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { ActivatedRoute } from "@angular/router";
+import { DestinationService } from '../destination/destination.service';
 
 @Component({
   selector: 'app-specific-destination',
@@ -9,17 +10,40 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class SpecificDestinationComponent implements OnInit {
   @Input() item: {};
- 
+  errorMessage: string;
+  destinations: any;
   region: string;
+  countries: any;
 
-  constructor(public route: ActivatedRoute) {
+  constructor(
+    public route: ActivatedRoute,
+    private destinationApiService: DestinationService) {
     this.route.params.subscribe(params => {
       this.region = params.region;
     });
+    this.destinations = [];
    }
 
   ngOnInit() {
-    console.log("Region: " + this.region);
+    console.log("Region: ." + this.region + ".");
+    this.getRegion();
   }
+  getRegion(): void {
+    this.destinationApiService.getDestination()
+      .subscribe(
+        destinations => {
+         for(let obj in destinations){
+           if(obj == this.region) {
+             this.countries = destinations[obj].Countries;
+           }
+         }
+
+         console.log(JSON.stringify(this.countries));
+        },
+        error => {
+          this.errorMessage = <any>error;
+        });
+  }
+
 
 }
